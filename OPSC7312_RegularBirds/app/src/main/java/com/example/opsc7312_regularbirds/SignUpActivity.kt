0cf2cd9f908b0c0b18dd3c884cead94aef60a9ba
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-08-27 09:25:14 +0200

    created sign in and sign up page

---
+++ OPSC7312_RegularBirds/app/src/main/java/com/example/opsc7312_regularbirds/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.opsc7312_regularbirds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
