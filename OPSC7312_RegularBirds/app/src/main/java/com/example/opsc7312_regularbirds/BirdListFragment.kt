71ce4a0bf0ea02dc22ff9f985117d68b17857572
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-11-08 07:57:45 +0200

    Added swipe refresh so pictures load

--- OPSC7312_RegularBirds/app/src/main/java/com/example/opsc7312_regularbirds/BirdListFragment.kt
+++ OPSC7312_RegularBirds/app/src/main/java/com/example/opsc7312_regularbirds/BirdListFragment.kt
@@ -7,8 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 
 class BirdListFragment : Fragment(), RVadapter_Observations.OnItemClickListener {
@@ -23,7 +27,10 @@ class BirdListFragment : Fragment(), RVadapter_Observations.OnItemClickListener 
     ): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_bird_list, container, false)
+        val swipeRefreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.swipe_refresh_layout)
+
         BirdObservationList= BirdObservationHandler.userDataX
+
         recyclerView = view.findViewById(R.id.lvObservartions) // Initialize recyclerView
         recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         recyclerViewAdapter = RVadapter_Observations(BirdObservationList)
@@ -39,6 +46,11 @@ class BirdListFragment : Fragment(), RVadapter_Observations.OnItemClickListener 
 
             activity.setCurrentFragment(forthFragment)
         }
+        //refresh page
+        swipeRefreshLayout.setOnRefreshListener {
+            refresh()
+            swipeRefreshLayout.isRefreshing = false
+        }
         return view
     }
 
@@ -46,6 +58,31 @@ class BirdListFragment : Fragment(), RVadapter_Observations.OnItemClickListener 
         val bottomSheet = Popup_ObservationDetails()
         BirdObservationHandler.setSelectedObservation(BirdObservationHandler.getObservationById(itemId)!!)
         bottomSheet.show(getChildFragmentManager(), "MyBottomSheet")
+    }
+
+    fun refresh() {
+        // Clear your existing data
+        //yourData.clear()
+
+        // Load your data again
+        loadData()
+    }
+
+    fun loadData() {
+
+        lifecycleScope.launch {
+
+            val job1 = async { BirdObservationHandler.getUserObservationsFromFireStore() }
+            val job2 = async { BirdObservationHandler.getImagesFromFireStore() }
+
+            try {
+                // Await the completion of all jobs
+                job1.await()
+                job2.await()
+            } catch (e: Exception) {
+                // Handle the exception
+            }
+        }
     }
 
 
